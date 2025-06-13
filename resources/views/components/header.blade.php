5888eb00f152b5c2b3f98f800ab12ff5333958e4
Jay <jayvallespin51@gmail.com>
2025-06-13 10:56:02 +0800

    feat: Add landing page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,13 @@
+<header class="h-14 lg:h-16 p-4 lg:px-6 border-b border-gray-200 flex items-center justify-between">
+  <div class="text-2xl font-bold text-orange-500">Talevy</div>
+  <nav>
+    <ul class="flex items-center gap-4">
+      <li>
+        <a href="/login" class="font-medium">Login</a>
+      </li>
+      <li>
+        <a href="/register" class="font-medium">Register</a>
+      </li>
+    </ul>
+  </nav>
+</header>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <div class="max-w-[1440px] mx-auto flex flex-col h-svh">
+    @yield('content')
+  </div>
+</body>
+
+</html>
