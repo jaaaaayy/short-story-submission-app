6f696e6f20ce1ad13787bc0f5cbe357be929a591
Orvilleson Tan <easalortan6@gmail.com>
2025-06-15 13:29:07 +0800

    feat: Add old values and input error messages

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -19,9 +19,12 @@
 
       <div class="grid gap-2">
         <label for="username" class="font-medium">Username</label>
-        <input type="text" id="username" name="username"
+        <input type="text" id="username" name="username" value="{{ old('username') }}"
           class="h-10 border border-gray-200 p-2 px-3 rounded-sm focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
           placeholder="Enter your username" autocomplete="off">
+        @error('username')
+          <p class="text-red-600">{{ $message }}</p>
+        @enderror
       </div>
 
       <div class="grid gap-2">
@@ -29,6 +32,9 @@
         <input type="password" id="password" name="password"
           class="h-10 border border-gray-200 p-2 px-3 rounded-sm focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
           placeholder="Enter your password">
+        @error('password')
+          <p class="text-red-600">{{ $message }}</p>
+        @enderror
       </div>
 
       <button
