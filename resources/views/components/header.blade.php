908079b35114e3c4b130d3feea10d4716a998000
Orvilleson Tan <easalortan6@gmail.com>
2025-06-15 16:56:33 +0800

    refactor: Make header sticky

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,5 @@
-<header class="h-14 lg:h-16 p-4 lg:px-6 border-b border-gray-200 flex items-center justify-between">
+<header
+  class="sticky top-0 bg-white h-14 lg:h-16 p-4 lg:px-6 border-b border-gray-200 flex items-center justify-between">
   <a href={{ route('landing.index') }} class="text-2xl font-bold text-orange-500">Talevy</a>
   <nav>
     <ul class="flex items-center gap-4">
