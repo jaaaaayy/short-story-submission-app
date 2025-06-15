710ed56bd3a350570d8cb15d9c42b48c40d5b618
Orvilleson Tan <easalortan6@gmail.com>
2025-06-15 17:24:43 +0800

    refactor: Make alert component dynamic

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,10 +1,10 @@
 @php
   $status = [
-      'success' => 'bg-green-500',
-      'error' => 'bg-red-500',
+      'success' => 'bg-green-400',
+      'error' => 'bg-red-400',
   ];
 @endphp
 
-<div class="flex items-center {{ $status[$type] }} text-white font-medium px-4 py-3 rounded-sm">
+<div class="flex items-center {{ $status[$type] }} text-white font-medium px-4 py-3 rounded-xs mx-4 lg:mx-6">
   <p>{{ $message }}</p>
 </div>
