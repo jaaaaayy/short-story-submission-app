83f8582f58c5ccfc6ef7abcdd754a7059c9f72f2
Orvilleson Tan <easalortan6@gmail.com>
2025-06-15 16:55:58 +0800

    feat: Add register functionality

--- resources/views/auth/register/index.blade.php
+++ resources/views/auth/register/index.blade.php
@@ -5,34 +5,40 @@
 @section('content')
 
   <div class="p-4 lg:p-6 grow flex items-center justify-center">
-    <form action="" class="grid gap-4 border border-gray-200 rounded-sm p-6 w-[400px]">
-      @if (session('error'))
-        <div class="flex items-center bg-red-500 text-white font-medium px-4 py-3 rounded-sm">
-          <p>{{ session('error') }}</p>
-        </div>
-      @endif
+    <form action="{{ route('auth.register') }}" method="POST"
+      class="grid gap-4 border border-gray-200 rounded-sm p-6 w-[400px]">
+      @csrf
 
       <h1 class="text-xl font-semibold text-center">Create an account.</h1>
 
       <div class="grid gap-2">
         <label for="username" class="font-medium">Username</label>
-        <input type="text" id="username"
+        <input type="text" id="username" name="username" value="{{ old('username') }}"
           class="h-10 border border-gray-200 p-2 px-3 rounded-sm focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
-          placeholder="Enter your username">
+          placeholder="Enter your username" autocomplete="off">
+        @error('username')
+          <p class="text-red-600">{{ $message }}</p>
+        @enderror
       </div>
 
       <div class="grid gap-2">
         <label for="email" class="font-medium">Email</label>
-        <input type="text" id="email"
+        <input type="text" id="email" name="email" value="{{ old('email') }}"
           class="h-10 border border-gray-200 p-2 px-3 rounded-sm focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
-          placeholder="Enter your email">
+          placeholder="Enter your email" autocomplete="off">
+        @error('email')
+          <p class="text-red-600">{{ $message }}</p>
+        @enderror
       </div>
 
       <div class="grid gap-2">
         <label for="password" class="font-medium">Password</label>
-        <input type="password" id="password"
+        <input type="password" id="password" name="password"
           class="h-10 border border-gray-200 p-2 px-3 rounded-sm focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
           placeholder="Enter your password">
+        @error('password')
+          <p class="text-red-600">{{ $message }}</p>
+        @enderror
       </div>
 
       <div class="grid gap-2">
