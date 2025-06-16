c6d6dffd16e7e4a9a9a75ecea97e99f172afab4d
Eugene Solamillo <eugenesolamillo.25@gmail.com>
2025-06-16 18:35:35 +0800

    feat: Create genres in GenreSeeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,31 @@ class GenreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+      $genres = [
+            'Adventure',
+            'Romance',
+            'Mystery',
+            'Horror',
+            'Science Fiction',
+            'Fantasy',
+            'Historical',
+            'Drama',
+            'Thriller',
+            'Comedy',
+            'Slice of Life',
+            'Supernatural',
+            'Crime',
+            'Dystopian',
+            'Coming-of-Age',
+            'Satire',
+            'Fairy Tale',
+            'Mythology',
+            'War',
+            'Tragedy',
+        ];
+
+        foreach ($genres as $name) {
+            Genre::create(['name' => $name]);
+        }
     }
 }
