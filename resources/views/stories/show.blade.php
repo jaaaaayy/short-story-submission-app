b50a0be9c7ab0de6c02edf72a4ba439d3b0709d4
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-17 21:54:30 +0800

    feat: Display author

--- resources/views/stories/show.blade.php
+++ resources/views/stories/show.blade.php
@@ -8,7 +8,8 @@
     <div class="w-[800px] flex flex-col gap-6 items-center">
       <div class="text-center space-y-1">
         <h1 class="text-xl font-semibold">{{ $story->title }}</h1>
-        <span>{{ $story->genre->name }}</span>
+        <p>{{ $story->genre->name }}</p>
+        <p>By <span class="font-medium">{{ $story->author->username }}</span></p>
       </div>
       <p>{!! nl2br(e($story->content)) !!}</p>
     </div>
