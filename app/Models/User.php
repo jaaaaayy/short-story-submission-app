c0cb7e5f2149e00321cb847407102202ae2c18ee
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-17 17:20:03 +0800

    feat: Implement user and story relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function stories()
+    {
+        return $this->hasMany(Story::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
