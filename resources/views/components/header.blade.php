afceebc4780f2aade196905a5ab0ed64259f7b11
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-17 22:16:50 +0800

    fix: Correct the route of brand link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header
   class="z-10 sticky top-0 bg-white h-14 lg:h-16 p-4 lg:px-6 border-b border-gray-200 flex items-center justify-between">
-  <a href={{ auth()->check() ? route('stories.index') : route('auth.index') }}
+  <a href={{ auth()->check() ? route('stories.index') : route('landing.index') }}
     class="text-2xl font-bold text-orange-500">Talevy</a>
   <nav>
     <ul class="flex items-center gap-4">
