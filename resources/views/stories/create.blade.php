304a47fdd984b77f542aadcdf62e1f2e9f6ff8b6
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-18 10:50:54 +0800

    refactor: Make write story responsive

--- resources/views/stories/create.blade.php
+++ resources/views/stories/create.blade.php
@@ -4,19 +4,20 @@
 
 @section('content')
 
-  <div class="p-4 lg:p-6 grow flex items-center justify-center">
+  <div class="p-4 lg:p-6 flex justify-center">
     <form action="{{ route('stories.store') }}" method="POST" enctype="multipart/form-data"
-      class="grid gap-4 border border-gray-200 rounded-xs p-6 w-[1200px]">
+      class="grid gap-4 border border-gray-200 rounded-xs p-4 lg:p-6 w-[1200px]">
       @csrf
 
       <h1 class="text-xl font-semibold text-center">Write a story.</h1>
 
-      <div class="grid grid-cols-3 gap-4">
+      <div class="grid lg:grid-cols-3 gap-4">
         <div class="flex flex-col gap-2">
           <label for="cover_image" class="font-medium">Cover Image</label>
           <input type="file" id="cover_image" name="cover_image" accept="image/*" class="hidden"
             onChange="previewCoverImage(event)" />
-          <div class="w-64 aspect-[1/1.5] border border-gray-200 cursor-pointer rounded-xs overflow-hidden relative"
+          <div
+            class="w-60 sm:w-64 aspect-[1/1.5] border border-gray-200 cursor-pointer rounded-xs overflow-hidden relative"
             onclick="document.getElementById('cover_image').click()">
             <img id="preview" class="w-full h-full object-cover hidden" />
             <div id="upload-placeholder" class="absolute inset-0 flex items-center justify-center text-neutral-500">
@@ -27,7 +28,7 @@
             <p class="text-red-500">{{ $message }}</p>
           @enderror
         </div>
-        <div class="col-span-2 space-y-4">
+        <div class="lg:col-span-2 space-y-4">
           <div class="grid gap-2">
             <label for="title" class="font-medium">Title</label>
             <input type="text" id="title" name="title" value="{{ old('title') }}"
