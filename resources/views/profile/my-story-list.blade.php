3a4772c806e9e62b84f256e2db6adb6b0fb3f878
Orvilleson Tan <easalortan6@gmail.com>
2025-06-18 14:21:42 +0800

    feat: Implement profile and my story list

---
+++ resources/views/profile/my-story-list.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+
+@section('title', $user->username . "'s story list | " . config('app.name'))
+
+@section('content')
+
+  <div class="flex grow">
+    <div class="p-4 lg:p-6 w-[300px]">
+      <x-sidebar />
+    </div>
+    <div class="p-4 lg:p-6 w-full space-y-4">
+      <h1 class="text-xl font-semibold">My Story List</h1>
+      <div class="grid grid-cols-4 md:grid-cols-8 gap-2">
+        @foreach ($stories as $story)
+          <a key={{ $story->id }} href="{{ route('stories.show', $story->id) }}"
+            class="rounded-xs overflow-hidden aspect-[16/25]">
+            <img src="{{ Storage::url($story->cover_image) }}" alt="{{ $story->title }}"
+              class="w-full h-full object-cover" />
+          </a>
+        @endforeach
+      </div>
+    </div>
+  </div>
+
+@endsection
