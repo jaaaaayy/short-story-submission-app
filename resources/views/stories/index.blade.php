0db303e595538d0d8de5856d416e8ed470bfcec4
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-18 10:32:50 +0800

    refactor: Make story list grid responsive for mobile

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -6,7 +6,7 @@
 
   <div class="p-4 lg:p-6 grow space-y-4">
     <h1 class="text-xl font-semibold">Story List</h1>
-    <div class="grid grid-cols-8 gap-4">
+    <div class="grid grid-cols-4 md:grid-cols-8 gap-2 lg:gap-4">
       @foreach ($stories as $story)
         <a key={{ $story->id }} href="{{ route('stories.show', $story->id) }}"
           class="rounded-xs overflow-hidden aspect-[16/25]">
