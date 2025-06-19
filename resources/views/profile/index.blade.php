f737f685cc8a297c1f9820c2645152144f224ce7
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-19 08:02:21 +0800

    feat: Implement profile information update

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,14 +8,31 @@
     <x-sidebar />
     <div class="p-4 lg:p-6 w-full space-y-4">
       <h1 class="text-xl font-semibold">Profile Information</h1>
-      <div class="grid gap-2">
-        <label for="username" class="font-medium">Username</label>
-        <p>{{ $user->username }}</p>
-      </div>
-      <div class="grid gap-2">
-        <label for="email" class="font-medium">Email</label>
-        <p>{{ $user->email }}</p>
-      </div>
+      <form action="{{ route('profile.update', $user->id) }}" method="POST">
+        @csrf
+        @method('PATCH')
+
+        <div class="grid gap-2">
+          <label for="username" class="font-medium">Username</label>
+          <input type="text" id="username" name="username" value="{{ $user->username }}"
+            class="h-10 border border-gray-200 p-2 px-3 rounded-xs focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
+            placeholder="Enter your username" autocomplete="off">
+          @error('username')
+            <p class="text-red-500">{{ $message }}</p>
+          @enderror
+        </div>
+        <div class="grid gap-2">
+          <label for="email" class="font-medium">Email</label>
+          <input type="text" id="email" name="email" value="{{ $user->email }}"
+            class="h-10 border border-gray-200 p-2 px-3 rounded-xs focus:outline-none focus:border-orange-500 focus:ring-orange-500/50 focus:ring-[3px]"
+            placeholder="Enter your email" autocomplete="off">
+          @error('email')
+            <p class="text-red-500">{{ $message }}</p>
+          @enderror
+        </div>
+        <button
+          class="h-10 bg-orange-500 hover:bg-orange-600 transition-colors p-2 px-3 rounded-xs text-white font-medium shadow-sm">Update</button>
+      </form>
     </div>
   </div>
 
